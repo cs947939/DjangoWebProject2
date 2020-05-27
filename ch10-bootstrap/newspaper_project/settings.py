16b2b0f7bbc6effaa6a5fd3e9e9d8ea096743476
cs947939 <sufam5@outlook.com>
2020-05-27 14:51:14 -0500

    more changes

--- ch10-bootstrap/newspaper_project/settings.py
+++ ch10-bootstrap/newspaper_project/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'DjangoWebProject2.urls'
+ROOT_URLCONF = 'newspaper_project.urls'
 
 TEMPLATES = [
     {
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'DjangoWebProject2.wsgi.application'
+WSGI_APPLICATION = 'newspaper_project.wsgi.application'
 
 
 # Database
@@ -132,5 +132,3 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
